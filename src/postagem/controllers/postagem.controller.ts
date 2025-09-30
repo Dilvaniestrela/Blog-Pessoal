a353f1a1c04d84cd017dbcef3418e837835024d0
dilvaniestrela <dilvani.estrela@gmail.com>
2025-09-30 11:36:41 -0300

    Atualização

---
+++ src/postagem/controllers/postagem.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe } from '@nestjs/common';
+import { Postagem } from '../entities/postagem.entity';
+import { PostagemService } from '../services/postagem.service';
+
+@Controller('postagens')
+export class PostagemController {
+  constructor(private readonly postagemService: PostagemService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async findAll(): Promise<Postagem[]> {
+    return this.postagemService.findAll();
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+    return this.postagemService.findById(id);
+  }
+
+  @Get('titulo/:titulo')
+  @HttpCode(HttpStatus.OK)
+  findAllByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+    return this.postagemService.findAllByTitulo(titulo);
+  }
+}
